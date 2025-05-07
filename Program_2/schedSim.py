634e34ec775038a85dc36adde94f8ff15c5cb30a
Hector Pule <hpule@unix4.csc.calpoly.edu>
2025-05-07 02:13:18 -0700

    Need to verify SRTN

--- Program_2/schedSim.py
+++ Program_2/schedSim.py
@@ -15,10 +15,6 @@ class Job:
         self.remaining_time = run_time
         self.start_time = -1  # Time when job first starts running
         self.finish_time = -1  # Time when job completes
-        self.is_started = False  # Flag to track if job has started running
-
-    def __str__(self):
-        return f"Job {self.id}: Run={self.run_time}, Arrival={self.arrival_time}, Remaining={self.remaining_time}"
 
 class SchedulerSimulator:
     def __init__(self, job_file, algorithm=ScheduleType.FIFO, quantum=1):
@@ -31,8 +27,6 @@ class SchedulerSimulator:
         self.completed_jobs = 0
         self.ready_queue = []
         
-        self.debug = True  # Set to False to disable all debug messages
-
     def read_job_file(self):
         try:
             with open(self.job_file, 'r') as file:
@@ -67,7 +61,6 @@ class SchedulerSimulator:
             job.remaining_time = job.run_time
             job.start_time = -1
             job.finish_time = -1
-            job.is_started = False
             
         if self.algorithm == ScheduleType.FIFO:
             self.simulate_fifo()
@@ -77,88 +70,97 @@ class SchedulerSimulator:
             self.simulate_rr()
         else:
             self.simulate_fifo()
-
-    def simulate_fifo(self):
-        print(f"Needs to be implementd")
-
-    def simulate_srtn(self):
-        if self.debug:
-            print("Running SRTN simulation")
+    
+    def run_job(self, job_index, run_time):
+        job = self.jobs[job_index]
         
+        if job.start_time == -1:
+            job.start_time = self.current_time
+        
+        old_time = self.current_time
+        self.current_time += run_time
+        job.remaining_time -= run_time
+        
+        # Check if job completed
+        if job.remaining_time == 0:
+            job.finish_time = self.current_time
+            self.completed_jobs += 1
+            return True  # Job completed
+        
+        return False  # Job not completed
+    
+    def get_available_jobs(self):
+        available_jobs = []
+        for i, job in enumerate(self.jobs):
+            if (job.arrival_time <= self.current_time and 
+                job.finish_time == -1):
+                available_jobs.append((i, job))
+        return available_jobs
+    
+    def arriving_job(self):
+        next_arrival = float('inf')
+        next_job_index = -1
+        
+        for i, job in enumerate(self.jobs):
+            if (job.arrival_time > self.current_time and 
+                job.arrival_time < next_arrival and 
+                job.finish_time == -1):
+                next_arrival = job.arrival_time
+                next_job_index = i
+        
+        return next_arrival
+    
+    def check_new_arrivals(self, old_time, current_job_index):
+        new_arrivals = []
+        
+        for i, job in enumerate(self.jobs):
+            if (job.arrival_time > old_time and 
+                job.arrival_time <= self.current_time and 
+                job.finish_time == -1 and 
+                i not in self.ready_queue and
+                i != current_job_index):
+                
+                self.ready_queue.append(i)
+                new_arrivals.append((i, job.arrival_time))
+        
+        return new_arrivals
+    
+    def simulate_srtn(self):        
         while self.completed_jobs < len(self.jobs):
-            shortest_job_index = -1
-            shortest_time = float('inf')
+            available_jobs = self.get_available_jobs()
             
-            # TODO: Loop through jobs and find the one with shortest remaining time
-            # that has arrived but not finished
-            for i, job
-            
-            # 2. If no jobs are ready, advance time
-            if shortest_job_index == -1:
+            if not available_jobs:
                 if not self.advance_to_next_arrival():
                     break  # No more jobs will arrive
                 continue
             
-            # 3. Get the job with shortest remaining time
-            job = self.jobs[shortest_job_index]
+            available_jobs.sort(key=lambda x: x[1].remaining_time)
+            job_index, job = available_jobs[0]
+            next_arrival = self.arriving_job()
             
-            # 4. Set start time if this is the first time running this job
-            if job.start_time == -1:
-                job.start_time = self.current_time
+            if next_arrival == float('inf'):
+                run_time = job.remaining_time  # Run until completion
+            else:
+                run_time = min(job.remaining_time, next_arrival - self.current_time)
             
-            # 5. Find when the next job will arrive (for preemption)
-            next_arrival = float('inf')
-            # TODO: Find the time of the next job arrival
-            
-            # 6. Determine how long to run the current job
-            # Either until completion or until next job arrives
-            run_time = 0  # TODO: Calculate run time
-            
-            # 7. Run the job for the calculated time
-            self.current_time += run_time
-            job.remaining_time -= run_time
-            
-            # 8. Check if job is completed
-            if job.remaining_time == 0:
-                job.finish_time = self.current_time
-                self.completed_jobs += 1
-
-    def simulate_rr(self):
+            self.run_job(job_index, run_time)
+    
+    def simulate_rr(self):        
         while self.completed_jobs < len(self.jobs):
             self.update_ready_queue()
             
-            if not self.ready_queue:  # No jobs ready, advance time
-                old_time = self.current_time
+            if not self.ready_queue:  # No jobs ready
                 if not self.advance_to_next_arrival():
                     break  # No more jobs
                 continue
             
             job_index = self.ready_queue.pop(0)
-            job = self.jobs[job_index]
-            
-            if job.start_time == -1:
-                job.start_time = self.current_time
-
-            run_time = min(self.quantum, job.remaining_time)
+            run_time = min(self.quantum, self.jobs[job_index].remaining_time)
             old_time = self.current_time
-            self.current_time += run_time
-            job.remaining_time -= run_time
+            job_completed = self.run_job(job_index, run_time)
+            self.check_new_arrivals(old_time, job_index)
             
-            arrival_times = []
-            for i, j in enumerate(self.jobs):
-                if (j.arrival_time > old_time and 
-                    j.arrival_time <= self.current_time and 
-                    j.finish_time == -1 and 
-                    i not in self.ready_queue and
-                    i != job_index):
-                    
-                    self.ready_queue.append(i)
-                    arrival_times.append((i, j.arrival_time))
-            
-            if job.remaining_time == 0:
-                job.finish_time = self.current_time
-                self.completed_jobs += 1
-            else:
+            if not job_completed:
                 self.ready_queue.append(job_index)
 
     def find_next_arrival_time(self):
@@ -183,31 +185,22 @@ class SchedulerSimulator:
                 i not in self.ready_queue):
                 self.ready_queue.append(i)
                 new_arrivals.append(i)
-        
-        if self.debug and new_arrivals:
-            print(f"New jobs added to ready queue: {new_arrivals}")
 
     def print_results(self):
-        total_TAT = 0.0
-        total_TW = 0.0
-        
-        print("\n----- Job Results -----")
-        # Sort jobs by ID to ensure they are printed in order
+        total_tat = 0.0
+        total_wt = 0.0
         sorted_jobs = sorted(self.jobs, key=lambda job: job.id)
-        
-        for job in sorted_jobs:
+        print("\n----- Job Results -----")
+        for job in self.jobs:
             tat = job.finish_time - job.arrival_time
             wt = tat - job.run_time
-            
-            print(f"Job {job.id:3d} -- TAT: {tat:3.2f} WT: {wt:3.2f}")
-            
-            total_TAT += tat
-            total_TW += wt
+            print(f"Job {job.id:3d} -- WT: {wt:3.1f} TAT: {tat:3.1f}")
+            total_tat += tat
+            total_wt += wt
         
-        avg_turnaround_time = total_TAT / len(self.jobs)
-        avg_wait_time = total_TW / len(self.jobs)
-        
-        print(f"Average TAT {avg_turnaround_time:3.2f} WT {avg_wait_time:3.2f}")
+        avg_tat = total_tat / len(self.jobs)
+        avg_wt = total_wt / len(self.jobs) 
+        print(f"Average WT:{avg_wt:3.1f} TAT:{avg_tat:3.1f} ")
 
 
 def parse_arguments():
@@ -226,9 +219,9 @@ def parse_arguments():
             try:
                 algorithm = ScheduleType(alg_str)
             except ValueError:
-                # If invalid algorithm provided, use default FIFO
-                algorithm = ScheduleType.FIFO
+                algorithm = ScheduleType.FIFO  #default FIFO
             i += 2
+
         elif sys.argv[i] == '-q' and i + 1 < len(sys.argv):
             try:
                 quantum_val = int(sys.argv[i + 1])
@@ -238,15 +231,13 @@ def parse_arguments():
                 pass
             i += 2
         else:
-            # Skip unknown arguments
-            i += 1
+            i += 1  # Skip unknown arguments
     
     return job_file, algorithm, quantum
 
 
 def main():
     job_file, algorithm, quantum = parse_arguments()
-    
     simulator = SchedulerSimulator(job_file, algorithm, quantum)
     simulator.read_job_file()
     simulator.assign_job_ids()
